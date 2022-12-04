1c0e53c488a95b996b18f8e81e8530882ea4d4ee
Matt Newton <mn00849@surrey.ac.uk>
2022-12-04 17:31:35 +0000

    Adding the Models

--- coursework/sportsapp/models.py
+++ coursework/sportsapp/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-'''
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    leader = models.ForeignKey(User, on_delete=models.SET_NULL)
+    leader = models.IntegerField()
 
 # Composite primary key
 class Member(models.Model):
-    userID = models.IntegerField()
-    teamID = models.IntegerField()
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    teamID = models.ForeignKey(Team, on_delete=models.CASCADE)
+    teamAdmin = models.BooleanField(default=False)
 
-'''
-
-'''
 class Fixture(models.Model):
     name = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
-    teamID = '''
+    date = models.DateTimeField()
+    description = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    teamID = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Avaliability(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    fixtureID = models.ForeignKey(Fixture, on_delete=models.CASCADE)
+    avaliable = models.BooleanField(default=False)
